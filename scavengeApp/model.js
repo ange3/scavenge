d81040da62d29d2a9372e668303bae3b924b7278
Puneeth Gadangi <pgadangi@stanford.edu>
2014-07-19 15:21:10 -0700

    commas

--- scavengeApp/model.js
+++ scavengeApp/model.js
@@ -3,16 +3,13 @@ Hunts = new Meteor.Collection("hunts");
 Hunts.allow({
 	insert: function() {
 		return true;
-	}
-
+	}, 
 	update: function() {
 		return true;
-	}
-
+	}, 
 	remove: function() {
 		return true;
-	}
-
+	}, 
 	get: function() {
 		return true;
 	}
