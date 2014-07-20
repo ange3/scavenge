7ba8bfdc081f3a051710f92a2e7ed838bff63673
Puneeth Gadangi <pgadangi@stanford.edu>
2014-07-20 00:34:27 -0700

    added score map

--- scavengeApp/client/hunter.js
+++ scavengeApp/client/hunter.js
@@ -4,13 +4,20 @@ Template.hunter.events({
 		var hunt = Hunts.findOne({_id : hunt_id});
 		if (hunt) {
 			Session.set("viewing_hunt", hunt._id);
+			var player = People.findOne({username: Session.get("user")});
+
+			var pscoremap = PlayerScoreMaps.findOne({user_id : player._id});
+			var curr_score = pscoremap[hunt_id];
+			var new_score = (curr_score) ? curr_score : 0;
+			var to_set = {};
+			to_set[hunt_id] = new_score;
+			PlayerScoreMaps.update(pscoremap._id, {$set : to_set});
 		}
 	}
 });
 
 Template.player_hunt_view.events({
 	'click button.start_hunt' : function() {
-		console.log('hi');
 		Session.set("hunt_started", 1);
 	}
 });
@@ -28,13 +35,16 @@ first_task_in_hunt = function(id) {
 }
 
 Template.player_hunt_view.task_name = function() {
+	if (!Session.get("viewing_hunt")) return "";
 	return first_task_in_hunt(Session.get("viewing_hunt")).name;
 }
 
 Template.player_hunt_view.task_description = function() {
+	if (!Session.get("viewing_hunt")) return "";
 	return first_task_in_hunt(Session.get("viewing_hunt")).description;
 }
 
 Template.player_hunt_view.task_question = function() {
+	if (!Session.get("viewing_hunt")) return "";
 	return first_task_in_hunt(Session.get("viewing_hunt")).question;
 }
